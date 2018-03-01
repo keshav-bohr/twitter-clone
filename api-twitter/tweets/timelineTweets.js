b89bb5db13113c3bac20f3f65e3869b7c28a4eb2
keshav0016 <keshav.b@westagilelabs.com>
2018-03-01 18:00:25 +0530

    Kwitter

--- api-twitter/tweets/timelineTweets.js
+++ api-twitter/tweets/timelineTweets.js
@@ -10,21 +10,28 @@ function timelineTweetsHandler(req, res, next){
     followModel.findOne({"user" : req.currentUser.id})
     .then(followRecord => {
         var tweetsOfFollowing = [];
-        tweetModel.find({user : { $in : followRecord.following}})
-        .then(requiredTweets => {
-            if(requiredTweets){
-                tweetsOfFollowing.push(...requiredTweets);
-                tweetsOfFollowing.sort(function(a, b){return a.createdAt - b.createdAt})
-                res.json({
-                    tweets : tweetsOfFollowing
-                })
-            }
-            else{
-                res.json({
-                    tweets : "no tweets found"
-                })
-            }
-        })
+        if(followRecord){
+            tweetModel.find({user : { $in : followRecord.following}})
+            .then(requiredTweets => {
+                if(requiredTweets){
+                    tweetsOfFollowing.push(...requiredTweets);
+                    tweetsOfFollowing.sort(function(a, b){return a.createdAt - b.createdAt})
+                    res.json({
+                        tweets : tweetsOfFollowing
+                    })
+                }
+                else{
+                    res.json({
+                        tweets : "no tweets found"
+                    })
+                }
+            })
+        }
+        else{
+            res.json({
+                tweets : "you have no followings"
+            })
+        }
     })
     .catch(error => {
         console.error(error);
